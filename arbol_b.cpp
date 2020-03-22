30b16486040d491d83278d399ba4da135307b24d
CarlaPerezGavilan <A01023033@itesm.mx>
2020-03-22 11:58:46 -0600

    la inserción sigue teniendo un problema cuando creece la raíz, y falta borrado

--- arbol_b.cpp
+++ arbol_b.cpp
@@ -3,6 +3,7 @@
 #include <time.h>
 #include<string.h>
 #include<vector>
+#include<tuple>
 #include <math.h>
 
 
@@ -15,8 +16,8 @@ using namespace std;
 template <class T>
 struct Node
 {
-    T key[N];// inicializarlos de forma ={0}
-    int posicion[N+1];
+    T key[N];
+    int nivel;
 };
 
 
@@ -34,16 +35,19 @@ FileManager(){
 }
 void leerArchivo(){
     fseek(archivo, 0, SEEK_SET);
-
+    int counter =0;
     if(!archivo) 
         printf("no existe archivo a leer"); 
     if(archivo != NULL){
         while(fread (&temp, sizeof(temp), 1, archivo)== 1){
+        cout<<"-----------"<<counter<<endl;
+
         for(int i=0; i<N; i++){
         printf ("nodo = %d\n", temp.key[i]);
         }
-        cout<<"-----------"<<endl;
+        counter++;
         }
+        
     }else{
         perror("No se puede abrir datos.dat");
     }
@@ -107,6 +111,7 @@ bool NodeFull(int index){
 
 void Insert(I in) 
 { 
+
     current_node= f.leerConIndice(current_index);
     //cuando no hay raiz 
     if(f.finalIndice()==0){
@@ -117,10 +122,17 @@ void Insert(I in)
 
    for(int i=0; i<N; i++){
       
-       if(current_node.key[i]> in || current_node.key[i]==0){
+       if(current_node.key[i]> in || current_node.key[i]==0 || N-1==i)
+       {
            if(nivel!=depth){
-                int child = getChildLeft(current_index+i);
-               current_index = child/N;
+            int child = 0;
+            if(current_node.key[i]<in  && current_node.key[i]!=0){
+                child= getChildRight(current_index+i);
+            }else{
+                child =getChildLeft(current_index+i);
+            }
+
+               current_index = child;
                nivel++;
                Insert(in);
                break;
@@ -128,6 +140,7 @@ void Insert(I in)
            }else{
                if(NodeFull(current_index)){
                    splitChild(current_index, in);
+                   
                }else{
                    insertNode(in, current_index);
                }
@@ -136,20 +149,13 @@ void Insert(I in)
                break;
                
            }
-       }else{
-           if(i==N-1){
-                splitChild(current_index, in);
-                current_index=0;
-                nivel=0;
-               break;
-           }
        }
 
     
-   } 
+    } 
+      }
       
-      
-    }
+    
 }
 
 void insertNode(I in, int where_insert){
@@ -158,6 +164,7 @@ void insertNode(I in, int where_insert){
     vec.push_back(in);
     sort(vec.begin(), vec.end());
     n = arrayToNode(vec); 
+   
     f.escribirRegistro(where_insert, n);
     
 }   
@@ -180,8 +187,6 @@ Node<I> arrayToNode(vector<I> vec){
     {
         if(i<vec.size()){
         temporal.key[i]= vec.at(i);
-        temporal.posicion[i]=(i+1)*N;
-        temporal.posicion[i+1]=(i+2)*N;
         }else{
         temporal.key[i]= 0;
         }
@@ -204,51 +209,92 @@ void splitChild(int index_to_split, I to_insert)
     for(int j= N/2+1; j<=N; j++){
         bigger->key[counter++]= vector_temp.at(j);
     }
-    f.escribirRegistro(index_to_split, childFul);
-    moveAll(1,index_to_split+1);
-    f.escribirRegistro(index_to_split+1, *bigger);
-    int p = getParentRight(index_to_split);
+
+     int p =0;
+    if(index_to_split%(N+1)==0){
+        p = getParentLeft(index_to_split);
+    }else{
+        p = getParentRight(index_to_split);
+    }
+
+   if(Occupied(p)==-1 && p%N != N-1){
+        f.escribirRegistro(index_to_split, childFul);
+        moveAll(1,index_to_split+1);
+        f.escribirRegistro(index_to_split+1, *bigger);
+   }
+    
+    
 
     if(p!=-1){
-        if(NodeFull(p/N)){ 
+        
+        if(NodeFull(p/N)){
             splitChild(p/N, vector_temp.at(N/2));
         }else{
-            if(NotOccupied(p)){
-                insertNode(vector_temp.at(N/2), p/N);
-            }else{
-                insertNode(specialSplit(index_to_split, to_insert, p), p/N);
+           if(Occupied(p)!=-1 || p%N == N-1){
+               if(p%N==N-1){
+                   p--;
+               }
+            int child =0;
+            if(p%N == 0){
+                child= getChildRight(p);
+            }  else{
+                child= getChildLeft(p);
             }
+            
+            if(NodeFull(child)==0){
+                int get_down = specialSplit(vector_temp, p, index_to_split, Occupied(p));
+               Node<I> temporal= f.leerConIndice(p/N);
+               temporal.key[p%N]=0;
+               f.escribirRegistro(p/N, temporal);
+               insertNode(get_down, p/N);
+            }else{
+                
+                insertNode(vector_temp.at(N/2), p/N);
+           
+                f.escribirRegistro(index_to_split, childFul);
+              
+                moveAll(1,index_to_split+1);
+                f.escribirRegistro(index_to_split+1, *bigger);
+
+            } 
+               
+           }else{
+                insertNode(vector_temp.at(N/2), p/N);
+           }     
         }
     }else{
+        
         depth++;
         fillInOthers();
         nivel--;
         Node<I> *root_new = new Node<I>();
         root_new->key[0] = vector_temp.at(N/2);
         moveAll(1, index_to_split);
-        f.escribirRegistro(index_to_split, *root_new);
-                
+       
+        f.escribirRegistro(index_to_split, *root_new);          
     }
 }
 
+int specialSplit(vector<I> vector_temp, int p, int index_to_split, int parent_index){
+    int return_value=0;
+     Node<I> node_insertion;
 
-int specialSplit(int index_to_split, I to_insert, int p){
-    Node<I> childFul = f.leerConIndice(index_to_split);
-    vector<I> vector_temp = nodeToArray(childFul);
-    vector_temp.push_back(to_insert);
-    sort(vector_temp.begin(), vector_temp.end());
-    insertNode(to_insert, getChildLeft(p)) ;
-    return vector_temp.at(0);
-}
-bool NotOccupied(int p){
-    int index = p%N;
-    int i = p/N;
-    Node<I> n= f.leerConIndice(i);
-    if(n.key[index]==0){
-        return true;
+    if(p%N==0){
+        for(int i=0; i<N; i++){
+        node_insertion.key[i]=vector_temp.at(i);
+        }   
+        
+        insertNode(parent_index, getChildRight(p));
+        return_value=vector_temp.at(N);
     }else{
-        return false;
-    }
+        for(int i=1; i<=N; i++){
+        node_insertion.key[i-1]=vector_temp.at(i);
+        }   
+        insertNode(parent_index, getChildLeft(p)) ;
+        return_value=vector_temp.at(0);
+    }   
+    f.escribirRegistro(index_to_split, node_insertion);
+    return return_value;
 }
 
 void fillInOthers(){
@@ -259,14 +305,25 @@ void fillInOthers(){
     }
 }
 
+int Occupied(int p){
+    int index = p%N;
+    int i = p/N;
+    Node<I> n= f.leerConIndice(i);
+    if(n.key[index]==0 || p==-1){
+        return -1;
+    }else{
+        return n.key[index];
+    }
+}
+
 void moveAll(int no_move, int initial){
     Node<I> n;
-    for(int i= getLast(depth); i>=initial; i--){
+    for(int i= getLast(depth)-no_move; i>=initial; i--){
         n = f.leerConIndice(i);
         f.escribirRegistro(i+no_move, n);
     }
     n= initialize(n);
-    for(int i=initial; i<no_move; i++){
+    for(int i=initial; i<initial+no_move; i++){
         f.escribirRegistro(i, n);
     }
 }
@@ -279,6 +336,9 @@ int getLast(int d){
     return acum-1;
 }
 
+int getFirst(int d){
+    return getLast(d-1)+1;
+}
 Node<I> initialize(Node<I> node){
     for(int i=0; i<N; i++){
         node.key[i]=0;
@@ -288,11 +348,11 @@ Node<I> initialize(Node<I> node){
 
 int getParentRight(int index)
 {
-if(index<N){
+if(index==0){
     return -1;
 }
  if(index%(N+1)==0){
-     return -1;
+        return -1;
  }else{
      return ((N*index)-1)/(N+1);
 
@@ -301,7 +361,7 @@ if(index<N){
 
 
 int getParentLeft(int index){
-    if(index<N){
+    if(index==0){
     return -1;
     }
     if(index%(N+1)==1){
@@ -317,6 +377,7 @@ int getChildLeft(int index){
     if(result>getLast(depth)){
         return -1;
     }
+    return result;
 }
 
 int getChildRight(int index){
@@ -327,17 +388,45 @@ int getChildRight(int index){
     return left+1;
 }
 
+int Buscar(int number_to_search){
+    current_node= f.leerConIndice(current_index);
+    for(int i=0; i<N; i++){
+        if(current_node.key[i]==number_to_search){
+            return (N*current_index)+i;
+        }else if(current_node.key[i]>number_to_search || current_node.key[i]==0){
+            current_index= getChildLeft(current_index+i);
+            return Buscar(number_to_search);
+        }
+    }
+    current_index=0;
+    current_node= f.leerConIndice(current_index);
+    return -1;
+}
+
 
 };
 
+
 int main(){
     BTree<int> arb;
-    int insert[14] = {10, 20, 65, 4, 5, 8, 12, 18, 25, 56, 70, 75, 80, 85};
-    for(int i=0; i<14; i++){
-        arb.Insert(insert[i]);
+    int array_insert[19] = {10, 20, 65, 90, 4, 5, 25, 56, 57, 60, 70, 75, 12, 18, 80, 85, 94, 95, 8};
+    for(int i=0; i<19; i++){
+        arb.Insert(array_insert[i]);
     }
+
+
+
+  
+
     cout<<"this is the result"<<endl;
     arb.f.leerArchivo();
+
+    cout<<" searching for 90, found in "<<arb.Buscar(90)<<endl;
+        cout<<" searching for 10, found in "<<arb.Buscar(10)<<endl;
+        cout<<" searching for 11, found in "<<arb.Buscar(11)<<endl;
+
+
+
 
     return 0;
 }
